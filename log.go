353408de53a544d69a54ab9eabf6c37e68966a49
Jamal Abdelkhalek <jamal.abdelkhalek@bobst.com>
2023-07-03 07:36:53 +0200

    Output logs file to a folder

--- log.go
+++ log.go
@@ -10,10 +10,10 @@ import (
 // LogToFile creates a file with a given suffix and writes the content into it.
 func LogToFile(suffix, content string) {
 	// Get current time and format it as a string
-	timestamp := time.Now().Format("20060102-150405") // "yyyyMMdd-HHmmss"
+	timestamp := time.Now().Format("20060102-150405.11.000") // "yyyyMMdd-HHmm.ss.sss"
 
 	// Create a new file with the timestamp and suffix in the name
-	file, fileErr := os.Create(fmt.Sprintf("%s-download-%s.txt", timestamp, suffix))
+	file, fileErr := os.Create(fmt.Sprintf("output-logs\\%s-download-%s.txt", timestamp, suffix))
 	if fileErr != nil {
 		fmt.Println("Failed to create log file:", fileErr)
 		return
